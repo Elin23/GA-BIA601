21ce515d9479d5a5c453e0262b6beb0644e45200
hiba alzain <hibaalzain81@gmail.com>
2025-10-31 00:07:44 +0300

    fix random generat for dataset

--- backend/generate_random_dataset.py
+++ backend/generate_random_dataset.py
@@ -1,12 +1,14 @@
+import random
 from sklearn.datasets import make_classification
 def generate_dataset():
+    seed = random.randint(0, 10000)
     X, y = make_classification(
     n_samples=500,
     n_features=40,
     n_informative=6,
     n_redundant=2,
     n_classes=2,
-    random_state=42
+    random_state=seed
     )
     result = X,y
     return result
